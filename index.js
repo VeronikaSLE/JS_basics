a7e72e9272cd6198e5ee2d550db5e1ff46e0f834
VeronikaSLE <118669112+VeronikaSLE@users.noreply.github.com>
2022-12-14 21:59:00 +0400

    [js] условные переменные

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
-// 1 переменные
-const lastName = 'Slesareva';
-let job = 'x5'
-let age = 28;
+// документацию искать со словом mdn
 
-console.log(age)
+// 1 переменные
+// const lastName = 'Slesareva';
+// let job = 'x5'
+// let age = 28;
+// console.log(age)
+
+// 2 мутирование
+// const firstName = prompt('Введите фамилию')
+// alert(lastName + ' ' + firstName)
+
+// 3 операторы
+// const currentYear = 2022
+// const birthYear = 1994
+
+// const age = currentYear - birthYear
+
+// console.log(age)
+
+// 4 типы данных
+//  const a = true
+//  const b = 'Veronika'
+//  const c = 28
+//  let x
+
+//  console.log(typeof a)
+//  console.log(typeof b)
+//  console.log(typeof c)
+//  console.log(typeof x)
+//  console.log(null)
+
+// 5 приоритет операторов
+// const fullAge = 26
+// const birthYear = 1994
+// const currentYear = 2022
+// const isFullAge = currentYear - birthYear >= fullAge
+// console.log(isFullAge)
+
+// 6 условные операторы
+// const corseStatus = 'pending'
+
+// if (corseStatus === 'ready') {
+//   console.log('Курс уже готов и его можно проходить')
+// }
+// else if (corseStatus === 'pending') {
+//   console.log('Курс пока в процессе разработки')
+// } 

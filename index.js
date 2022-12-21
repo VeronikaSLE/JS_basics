ed367e295a0e49472fd8c3aa591defdb8444a7c7
VeronikaSLE <118669112+VeronikaSLE@users.noreply.github.com>
2022-12-21 23:39:29 +0400

    [js] Base finish

--- index.js
+++ index.js
@@ -46,3 +46,53 @@
 // else if (corseStatus === 'pending') {
 //   console.log('Курс пока в процессе разработки')
 // } 
+
+// 7 булевая логика
+// && и
+// || или
+
+// 8 функции
+// function calculateAge(year) {
+//   console.log('year: ', year);
+//   const result = 2022 - year;
+//   return result
+// }
+
+// const myAge = calculateAge(1994)
+// console.log(myAge)
+
+// function logInfoAbout(name, year) {
+//   const age = calculateAge(year)
+
+//   console.log('меня зовут '+ name + ' мне '+year);
+//   console.log(`меня зовут ${name} мне сейчас ${year}`);
+// }
+// logInfoAbout('Veronika', 1994)
+
+// 9 массивы
+// const cars = ['mazda', 'benz', 'ford']
+
+// 10 циклы
+// const cars = ['mazda', 'benz', 'ford', 'bmw the best car'];
+// for (let i = 0; i < cars.length; i++) {
+//   const car = cars[i];
+//   console.log(car)
+// }
+
+// cars.map((car) => {
+//   console.log(car)
+// });
+
+// 11 объекты
+// const person = {
+//   firstName: 'Veronika',
+//   lastName: 'Slesareva',
+//   year: 1994,
+//   langs: ['ru', 'en', 'de'],
+//   hello: function() {
+//     console.log(`привет, я ${this.firstName} ${this.lastName} 
+//     я ${this.year} года рождения`)
+//   }
+// }
+// person.hello()
+// console.log(person.langs);
